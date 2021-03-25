e2ccf5489dcb35f1ea3c7618746e8a599c52eb06
NVV <uniondirfolder@gmail.com>
2021-03-25 10:20:06 -0700

    Add project files.

---
+++ wa-mvc-itstep-hometask-News-Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wa_mvc_itstep_hometask_News_Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
