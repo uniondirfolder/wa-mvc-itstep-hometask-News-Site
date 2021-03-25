5d46580bbde40169a09236cac72965bdec7420bc
NVV <uniondirfolder@gmail.com>
2021-03-25 13:14:58 -0700

    EndWithStartFile

--- wa-mvc-itstep-hometask-News-Site/Startup.cs
+++ wa-mvc-itstep-hometask-News-Site/Startup.cs
@@ -1,18 +1,25 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using wa_mvc_itstep_hometask_News_Site.Models;
+using wa_mvc_itstep_hometask_News_Site.Services.Parsing;
+using wa_mvc_itstep_hometask_News_Site.Services.Rss;
+using wa_mvc_itstep_hometask_News_Site.Services.Validation;
+using wa_mvc_itstep_hometask_News_Site.Utils.Currency;
 
 namespace wa_mvc_itstep_hometask_News_Site
 {
     public class Startup
     {
+       
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,6 +30,22 @@ namespace wa_mvc_itstep_hometask_News_Site
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(Configuration.GetConnectionString("NVVPostgresConnection")));
+            
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = new PathString("/Account/Login");
+                    options.AccessDeniedPath = new PathString("/Account/Login");
+                });
+
+            services.AddScoped<EvilInfoRss>();
+            services.AddScoped<EvilInfoParsing>();
+            services.AddScoped<ValidationService>();
+            services.AddScoped<CurrencyCBER>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
             services.AddControllersWithViews();
         }
 
@@ -35,22 +58,45 @@ namespace wa_mvc_itstep_hometask_News_Site
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler(errorApp =>
+                {
+                    errorApp.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.ContentType = "text/html;charset=utf-8";
+
+                        await context.Response.WriteAsync("<html lang=\"en\"><body style=\"text-align: center;\">\r\n");
+                        await context.Response.WriteAsync("<h1>This page is unavailable now(</h1><br>\r\n");
+
+                        await context.Response.WriteAsync("<a href=\"/\"><h3>Back to site</h3></a><br>\r\n");
+                        await context.Response.WriteAsync("</body></html>\r\n");
+                        await context.Response.WriteAsync(new string(' ', 512)); // IE padding
+                    });
+                });
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseCookiePolicy(new CookiePolicyOptions()
+            {
+                HttpOnly = HttpOnlyPolicy.Always,
+                MinimumSameSitePolicy = SameSiteMode.None,
+                Secure = CookieSecurePolicy.Always
+            });
+
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}");
             });
         }
     }
